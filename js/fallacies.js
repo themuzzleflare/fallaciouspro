ea1c89c3a016a540c1015d07060a11868748fded
Paul Tavitian <paul.tavitian@icloud.com>
2021-08-03 08:52:40 +1000

    Changes

--- js/fallacies.js
+++ js/fallacies.js
@@ -1,175 +1,175 @@
 CloudKit.configure({
-    containers: [{
-        containerIdentifier: "iCloud.cloud.tavitian.fallaciouspro",
-        apiTokenAuth: {
-            apiToken: "c3bf01204e15c98abe27839e3a38d321cf1f944a1b339e68616d745906787e87"
-        },
-        environment: "production"
-    }]
+  containers: [{
+    containerIdentifier: "iCloud.cloud.tavitian.fallaciouspro",
+    apiTokenAuth: {
+      apiToken: "c3bf01204e15c98abe27839e3a38d321cf1f944a1b339e68616d745906787e87"
+    },
+    environment: "production"
+  }]
 });
 
-var container = CloudKit.getDefaultContainer();
-var database = container.publicCloudDatabase;
+let container = CloudKit.getDefaultContainer();
+let database = container.publicCloudDatabase;
 
-fetchFallacies()
+fetchFallacies();
 
 function fetchFallacies() {
-    var query = {
-        recordType: "Fallacies",
-        sortBy: [
-            {
-                fieldName: "name",
-                ascending: true
-            }
-        ]
+  let query = {
+    recordType: "Fallacies",
+    sortBy: [
+      {
+        fieldName: "name",
+        ascending: true
+      }
+    ]
+  }
+  
+  let options = {
+    resultsLimit: 200
+  }
+  
+  database.performQuery(query, options)
+  .then(function(response) {
+    if (response.hasErrors) {
+      throw response.errors[0];
+    } else {
+      showRecords(response.records);
     }
-    
-    var options = {
-        resultsLimit: 200
-    }
-    
-    database.performQuery(query, options)
-    .then(function(response) {
-        if (response.hasErrors) {
-            throw response.errors[0];
-        } else {
-            showRecords(response.records);
-        }
-    });
-}
+  });
+};
 
 function showRecords(records) {
-    const recordsDiv = document.querySelector("#records")
+  const recordsDiv = document.querySelector("#records")
+  
+  const accordionElement = document.createElement("ul")
+  accordionElement.setAttribute("uk-accordion", "")
+  
+  records.forEach(record => {
+    const o = record.fields
     
-    const accordionElement = document.createElement("ul")
-    accordionElement.setAttribute("uk-accordion", "")
+    const accordionListElement = document.createElement("li")
+    const titleElement =  document.createElement("a")
     
-    records.forEach(record => {
-        const o = record.fields
-        
-        const accordionListElement = document.createElement("li")
-        const titleElement =  document.createElement("a")
-        
-        titleElement.classList.add("uk-accordion-title")
-        titleElement.innerText = o.name.value
-        
-        const t = document.createElement("div")
-        t.classList.add("uk-accordion-content")
-        
-        if (o.hasOwnProperty("name")) {
-            const e = document.createElement("p")
-            e.innerHTML = `<b>Name</b>: ${o.name.value}`
-            t.append(e)
-        }
-        
-        if (o.hasOwnProperty("latinName")) {
-            const e = document.createElement("p");
-            e.innerHTML = `<b>Latin Name</b>: ${o.latinName.value}`,
-            t.append(e)
-        }
-        
-        if (o.hasOwnProperty("aliases")) {
-            const e = o.aliases.value.join(", ").italics(),
-            n = document.createElement("p");
-            n.innerHTML = `<b>Aliases</b>: ${e}`,
-            t.append(n)
-        }
-        
-        if (o.hasOwnProperty("newTerminology")) {
-            const e = document.createElement("div"),
-            n = document.createElement("p");
-            n.innerHTML = "<b>New Terminology</b>:";
-            const a = document.createElement("ul");
-            o.newTerminology.value.forEach(e => {
-                const n = document.createElement("li");
-                n.innerText = `${e}`,
-                n.style.marginBottom = "1em",
-                a.append(n)
-            }),
-            e.append(n, a),
-            t.append(e)
-        }
-        
-        if (o.hasOwnProperty("logicalForms")) {
-            const e = document.createElement("div"),
-            n = document.createElement("p");
-            n.innerHTML = "<b>Logical Forms</b>:";
-            const a = document.createElement("ul");
-            o.logicalForms.value.forEach(e => {
-                const n = document.createElement("li");
-                n.innerText = `${e}`,
-                n.style.marginBottom = "1em",
-                a.append(n)
-            }),
-            e.append(n, a),
-            t.append(e)
-        }
-        
-        if (o.hasOwnProperty("description")) {
-            const n = document.createElement("p");
-            n.innerHTML = `<b>Description</b>: ${o.description.value}`,
-            t.append(n)
-        }
-        
-        if (o.hasOwnProperty("examples")) {
-            const e = document.createElement("div"),
-            n = document.createElement("p");
-            n.innerHTML = "<b>Examples</b>:";
-            const a = document.createElement("ul");
-            o.examples.value.forEach(e => {
-                const n = document.createElement("li");
-                n.innerText = `${e}`,
-                n.style.marginBottom = "1em",
-                a.append(n)
-            }),
-            e.append(n, a),
-            t.append(e)
-        }
-        
-        if (o.hasOwnProperty("exceptions")) {
-            const e = document.createElement("div"),
-            n = document.createElement("p");
-            n.innerHTML = "<b>Exceptions</b>:";
-            const a = document.createElement("ul");
-            o.exceptions.value.forEach(e => {
-                const n = document.createElement("li");
-                n.innerText = `${e}`,
-                n.style.marginBottom = "1em",
-                a.append(n)
-            }),
-            e.append(n, a),
-            t.append(e)
-        }
-        
-        if (o.hasOwnProperty("tip")) {
-            const e = document.createElement("p");
-            e.innerHTML = `<b>Tip</b>: ${o.tip.value}`,
-            t.append(e)
-        }
-        
-        if (o.hasOwnProperty("references")) {
-            const e = document.createElement("div"),
-            n = document.createElement("p");
-            n.innerHTML = "<b>References</b>:";
-            const a = document.createElement("ul");
-            o.references.value.forEach(e => {
-                const n = document.createElement("li");
-                n.innerText = `${e}`,
-                n.style.marginBottom = "1em",
-                a.append(n)
-            }),
-            e.append(n, a),
-            t.append(e)
-        }
-        
-        accordionListElement.style.backgroundColor = "silver"
-        accordionListElement.style.border = "thin solid #000000"
-        accordionListElement.style.borderRadius = "20px"
-        accordionListElement.style.margin = "10px"
-        accordionListElement.style.padding = "20px"
-        
-        accordionListElement.append(titleElement, t)
-        accordionElement.append(accordionListElement)
-    })
-    recordsDiv.append(accordionElement)
+    titleElement.classList.add("uk-accordion-title")
+    titleElement.innerText = o.name.value
+    
+    const t = document.createElement("div")
+    t.classList.add("uk-accordion-content")
+    
+    if (o.hasOwnProperty("name")) {
+      const e = document.createElement("p")
+      e.innerHTML = `<b>Name</b>: ${o.name.value}`
+      t.append(e)
+    }
+    
+    if (o.hasOwnProperty("latinName")) {
+      const e = document.createElement("p");
+      e.innerHTML = `<b>Latin Name</b>: ${o.latinName.value}`,
+      t.append(e)
+    }
+    
+    if (o.hasOwnProperty("aliases")) {
+      const e = o.aliases.value.join(", ").italics(),
+      n = document.createElement("p");
+      n.innerHTML = `<b>Aliases</b>: ${e}`,
+      t.append(n)
+    }
+    
+    if (o.hasOwnProperty("newTerminology")) {
+      const e = document.createElement("div"),
+      n = document.createElement("p");
+      n.innerHTML = "<b>New Terminology</b>:";
+      const a = document.createElement("ul");
+      o.newTerminology.value.forEach(e => {
+        const n = document.createElement("li");
+        n.innerText = `${e}`,
+        n.style.marginBottom = "1em",
+        a.append(n)
+      }),
+      e.append(n, a),
+      t.append(e)
+    }
+    
+    if (o.hasOwnProperty("logicalForms")) {
+      const e = document.createElement("div"),
+      n = document.createElement("p");
+      n.innerHTML = "<b>Logical Forms</b>:";
+      const a = document.createElement("ul");
+      o.logicalForms.value.forEach(e => {
+        const n = document.createElement("li");
+        n.innerText = `${e}`,
+        n.style.marginBottom = "1em",
+        a.append(n)
+      }),
+      e.append(n, a),
+      t.append(e)
+    }
+    
+    if (o.hasOwnProperty("description")) {
+      const n = document.createElement("p");
+      n.innerHTML = `<b>Description</b>: ${o.description.value}`,
+      t.append(n)
+    }
+    
+    if (o.hasOwnProperty("examples")) {
+      const e = document.createElement("div"),
+      n = document.createElement("p");
+      n.innerHTML = "<b>Examples</b>:";
+      const a = document.createElement("ul");
+      o.examples.value.forEach(e => {
+        const n = document.createElement("li");
+        n.innerText = `${e}`,
+        n.style.marginBottom = "1em",
+        a.append(n)
+      }),
+      e.append(n, a),
+      t.append(e)
+    }
+    
+    if (o.hasOwnProperty("exceptions")) {
+      const e = document.createElement("div"),
+      n = document.createElement("p");
+      n.innerHTML = "<b>Exceptions</b>:";
+      const a = document.createElement("ul");
+      o.exceptions.value.forEach(e => {
+        const n = document.createElement("li");
+        n.innerText = `${e}`,
+        n.style.marginBottom = "1em",
+        a.append(n)
+      }),
+      e.append(n, a),
+      t.append(e)
+    }
+    
+    if (o.hasOwnProperty("tip")) {
+      const e = document.createElement("p");
+      e.innerHTML = `<b>Tip</b>: ${o.tip.value}`,
+      t.append(e)
+    }
+    
+    if (o.hasOwnProperty("references")) {
+      const e = document.createElement("div"),
+      n = document.createElement("p");
+      n.innerHTML = "<b>References</b>:";
+      const a = document.createElement("ul");
+      o.references.value.forEach(e => {
+        const n = document.createElement("li");
+        n.innerText = `${e}`,
+        n.style.marginBottom = "1em",
+        a.append(n)
+      }),
+      e.append(n, a),
+      t.append(e)
+    }
+    
+    accordionListElement.style.backgroundColor = "silver"
+    accordionListElement.style.border = "thin solid #000000"
+    accordionListElement.style.borderRadius = "20px"
+    accordionListElement.style.margin = "10px"
+    accordionListElement.style.padding = "20px"
+    
+    accordionListElement.append(titleElement, t)
+    accordionElement.append(accordionListElement)
+  })
+  recordsDiv.append(accordionElement)
 };
--- js/fallacious-biases.js
+++ js/fallacious-biases.js
@@ -1,147 +1,147 @@
 CloudKit.configure({
-    containers: [{
-        containerIdentifier: "iCloud.cloud.tavitian.fallacious",
-        apiTokenAuth: {
-            apiToken: "0efa83e61396cb57ec2882998bb8c72fda8ebc1a4b6742bb252b300429b90b1f"
-        },
-        environment: "production"
-    }]
+  containers: [{
+    containerIdentifier: "iCloud.cloud.tavitian.fallacious",
+    apiTokenAuth: {
+      apiToken: "0efa83e61396cb57ec2882998bb8c72fda8ebc1a4b6742bb252b300429b90b1f"
+    },
+    environment: "production"
+  }]
 });
 
-var container = CloudKit.getDefaultContainer();
-var database = container.publicCloudDatabase;
+let container = CloudKit.getDefaultContainer();
+let database = container.publicCloudDatabase;
 
-fetchBiases()
+fetchBiases();
 
 function fetchBiases() {
-    var query = {
-        recordType: "itemData",
-        sortBy: [
-            {
-                fieldName: "id",
-                ascending: true
-            },
-            {
-                fieldName: "number",
-                ascending: true
-            }
-        ],
-        filterBy: [
-            {
-                comparator: "EQUALS",
-                fieldName: "category",
-                fieldValue: {
-                    value: {
-                        recordName: "cognitive-bias",
-                        action: "NONE"
-                    },
-                    type: "REFERENCE"
-                }
-            }
-        ]
-    }
-    
-    var options = {
-        desiredKeys: [
-            "id",
-            "number",
-            "name",
-            "categoryName",
-            "shortDescription",
-            "longDescription",
-            "example",
-            "image",
-            "isFeatured"
-        ],
-        resultsLimit: 200
-    }
-    
-    database.performQuery(query, options)
-    .then(function(response) {
-        if (response.hasErrors) {
-            throw response.errors[0];
-        } else {
-            showRecords(response.records);
+  let query = {
+    recordType: "itemData",
+    sortBy: [
+      {
+        fieldName: "id",
+        ascending: true
+      },
+      {
+        fieldName: "number",
+        ascending: true
+      }
+    ],
+    filterBy: [
+      {
+        comparator: "EQUALS",
+        fieldName: "category",
+        fieldValue: {
+          value: {
+            recordName: "cognitive-bias",
+            action: "NONE"
+          },
+          type: "REFERENCE"
         }
-    });
-}
+      }
+    ]
+  }
+  
+  let options = {
+    desiredKeys: [
+      "id",
+      "number",
+      "name",
+      "categoryName",
+      "shortDescription",
+      "longDescription",
+      "example",
+      "image",
+      "isFeatured"
+    ],
+    resultsLimit: 200
+  }
+  
+  database.performQuery(query, options)
+  .then(function(response) {
+    if (response.hasErrors) {
+      throw response.errors[0];
+    } else {
+      showRecords(response.records);
+    }
+  });
+};
 
 function showRecords(records) {
-    const recordsDiv = document.querySelector("#fallacious-biases");
+  const recordsDiv = document.querySelector("#fallacious-biases");
+  
+  const accordionElement = document.createElement("ul")
+  accordionElement.setAttribute("uk-accordion", "")
+  
+  records.forEach(record => {
+    const field = record.fields
     
-    const accordionElement = document.createElement("ul")
-    accordionElement.setAttribute("uk-accordion", "")
+    const accordionListElement = document.createElement("li")
+    const titleElement =  document.createElement("a")
     
-    records.forEach(record => {
-        const field = record.fields
-        
-        const accordionListElement = document.createElement("li")
-        const titleElement =  document.createElement("a")
-        
-        titleElement.classList.add("uk-accordion-title")
-        titleElement.innerText = field.name.value
-        
-        const recordElement = document.createElement("div");
-        recordElement.classList.add("uk-accordion-content")
-        
-        if (field.hasOwnProperty("image")) {
-            const imageElement = document.createElement("img");
-            
-            imageElement.src = `${field.image.value.downloadURL}`;
-            imageElement.width = "100";
-            imageElement.height = "100";
-            
-            recordElement.append(imageElement)
-        }
-        
-        if (field.hasOwnProperty("name")) {
-            const nameElement = document.createElement("p");
-            
-            nameElement.innerHTML = `<b>Name</b>: ${field.name.value}`;
-            
-            recordElement.append(nameElement)
-        }
-        
-        if (field.hasOwnProperty("categoryName")) {
-            const categoryNameElement = document.createElement("p");
-            
-            categoryNameElement.innerHTML = `<b>Category</b>: ${field.categoryName.value}`;
-            
-            recordElement.append(categoryNameElement)
-        }
-        
-        if (field.hasOwnProperty("shortDescription")) {
-            const shortDescriptionElement = document.createElement("p");
-            
-            shortDescriptionElement.innerHTML = `<b>Short Description</b>: ${field.shortDescription.value}`;
-            
-            recordElement.append(shortDescriptionElement)
-        }
-        
-        if (field.hasOwnProperty("longDescription")) {
-            const longDescriptionElement = document.createElement("p");
-            
-            longDescriptionElement.innerHTML = `<b>Long Description</b>: ${field.longDescription.value}`;
-            
-            recordElement.append(longDescriptionElement)
-        }
-        
-        if (field.hasOwnProperty("example")) {
-            const solutionElement = document.createElement("p");
-            
-            solutionElement.innerHTML = `<b>Solution</b>: ${field.example.value}`;
-            
-            recordElement.append(solutionElement)
-        }
-        
-        accordionListElement.style.backgroundColor = "silver"
-        accordionListElement.style.border = "thin solid #000000"
-        accordionListElement.style.borderRadius = "20px"
-        accordionListElement.style.margin = "10px"
-        accordionListElement.style.padding = "20px"
-        
-        accordionListElement.append(titleElement, recordElement)
-        accordionElement.append(accordionListElement)
-    });
-    recordsDiv.append(accordionElement);
-}
+    titleElement.classList.add("uk-accordion-title")
+    titleElement.innerText = field.name.value
+    
+    const recordElement = document.createElement("div");
+    recordElement.classList.add("uk-accordion-content")
+    
+    if (field.hasOwnProperty("image")) {
+      const imageElement = document.createElement("img");
+      
+      imageElement.src = `${field.image.value.downloadURL}`;
+      imageElement.width = "100";
+      imageElement.height = "100";
+      
+      recordElement.append(imageElement)
+    }
+    
+    if (field.hasOwnProperty("name")) {
+      const nameElement = document.createElement("p");
+      
+      nameElement.innerHTML = `<b>Name</b>: ${field.name.value}`;
+      
+      recordElement.append(nameElement)
+    }
+    
+    if (field.hasOwnProperty("categoryName")) {
+      const categoryNameElement = document.createElement("p");
+      
+      categoryNameElement.innerHTML = `<b>Category</b>: ${field.categoryName.value}`;
+      
+      recordElement.append(categoryNameElement)
+    }
+    
+    if (field.hasOwnProperty("shortDescription")) {
+      const shortDescriptionElement = document.createElement("p");
+      
+      shortDescriptionElement.innerHTML = `<b>Short Description</b>: ${field.shortDescription.value}`;
+      
+      recordElement.append(shortDescriptionElement)
+    }
+    
+    if (field.hasOwnProperty("longDescription")) {
+      const longDescriptionElement = document.createElement("p");
+      
+      longDescriptionElement.innerHTML = `<b>Long Description</b>: ${field.longDescription.value}`;
+      
+      recordElement.append(longDescriptionElement)
+    }
+    
+    if (field.hasOwnProperty("example")) {
+      const solutionElement = document.createElement("p");
+      
+      solutionElement.innerHTML = `<b>Solution</b>: ${field.example.value}`;
+      
+      recordElement.append(solutionElement)
+    }
+    
+    accordionListElement.style.backgroundColor = "silver"
+    accordionListElement.style.border = "thin solid #000000"
+    accordionListElement.style.borderRadius = "20px"
+    accordionListElement.style.margin = "10px"
+    accordionListElement.style.padding = "20px"
+    
+    accordionListElement.append(titleElement, recordElement)
+    accordionElement.append(accordionListElement)
+  });
+  recordsDiv.append(accordionElement);
+};
