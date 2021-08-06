3ad249b14ce67a4e9b6701ced8d864f3d84a9d46
Paul Tavitian <paul.tavitian@icloud.com>
2021-08-06 19:37:32 +1000

    Changes

--- js/fallacies.js
+++ js/fallacies.js
@@ -1,1 +1,153 @@
-"use strict";CloudKit.configure({containers:[{containerIdentifier:"iCloud.cloud.tavitian.fallaciouspro",apiTokenAuth:{apiToken:"c3bf01204e15c98abe27839e3a38d321cf1f944a1b339e68616d745906787e87"},environment:"production"}]});let container=CloudKit.getDefaultContainer(),database=container.publicCloudDatabase;function fetchFallacies(){database.performQuery({recordType:"Fallacies",sortBy:[{fieldName:"name",ascending:!0}]},{resultsLimit:200}).then(function(e){if(e.hasErrors)throw e.errors[0];showRecords(e.records)})}function showRecords(e){const n=document.querySelector("#records"),t=document.createElement("ul");t.setAttribute("uk-accordion",""),e.forEach(e=>{const n=e.fields,a=document.createElement("li"),o=document.createElement("a");o.classList.add("uk-accordion-title"),o.innerText=n.name.value;const c=document.createElement("div");if(c.classList.add("uk-accordion-content"),n.hasOwnProperty("name")){const e=document.createElement("p");e.innerHTML=`<b>Name</b>: ${n.name.value}`,c.append(e)}if(n.hasOwnProperty("latinName")){const e=document.createElement("p");e.innerHTML=`<b>Latin Name</b>: ${n.latinName.value}`,c.append(e)}if(n.hasOwnProperty("aliases")){const e=n.aliases.value.join(", ").italics(),t=document.createElement("p");t.innerHTML=`<b>Aliases</b>: ${e}`,c.append(t)}if(n.hasOwnProperty("newTerminology")){const e=document.createElement("div"),t=document.createElement("p");t.innerHTML="<b>New Terminology</b>:";const a=document.createElement("ul");n.newTerminology.value.forEach(e=>{const n=document.createElement("li");n.innerText=`${e}`,n.style.marginBottom="1em",a.append(n)}),e.append(t,a),c.append(e)}if(n.hasOwnProperty("logicalForms")){const e=document.createElement("div"),t=document.createElement("p");t.innerHTML="<b>Logical Forms</b>:";const a=document.createElement("ul");n.logicalForms.value.forEach(e=>{const n=document.createElement("li");n.innerText=`${e}`,n.style.marginBottom="1em",a.append(n)}),e.append(t,a),c.append(e)}if(n.hasOwnProperty("description")){const e=document.createElement("p");e.innerHTML=`<b>Description</b>: ${n.description.value}`,c.append(e)}if(n.hasOwnProperty("examples")){const e=document.createElement("div"),t=document.createElement("p");t.innerHTML="<b>Examples</b>:";const a=document.createElement("ul");n.examples.value.forEach(e=>{const n=document.createElement("li");n.innerText=`${e}`,n.style.marginBottom="1em",a.append(n)}),e.append(t,a),c.append(e)}if(n.hasOwnProperty("exceptions")){const e=document.createElement("div"),t=document.createElement("p");t.innerHTML="<b>Exceptions</b>:";const a=document.createElement("ul");n.exceptions.value.forEach(e=>{const n=document.createElement("li");n.innerText=`${e}`,n.style.marginBottom="1em",a.append(n)}),e.append(t,a),c.append(e)}if(n.hasOwnProperty("tip")){const e=document.createElement("p");e.innerHTML=`<b>Tip</b>: ${n.tip.value}`,c.append(e)}if(n.hasOwnProperty("references")){const e=document.createElement("div"),t=document.createElement("p");t.innerHTML="<b>References</b>:";const a=document.createElement("ul");n.references.value.forEach(e=>{const n=document.createElement("li");n.innerText=`${e}`,n.style.marginBottom="1em",a.append(n)}),e.append(t,a),c.append(e)}a.style.backgroundColor="silver",a.style.border="thin solid #000000",a.style.borderRadius="20px",a.style.margin="10px",a.style.padding="20px",a.append(o,c),t.append(a)}),n.append(t)}fetchFallacies();
+"use strict";
+
+CloudKit.configure({
+  containers: [{
+    containerIdentifier: "iCloud.cloud.tavitian.fallaciouspro",
+    environment: "production",
+    apiTokenAuth: {
+      apiToken: "e8783e437718ef1d781bef3981ad9eb106a5a8451e546aa4cd121ab6207eeebf"
+    }
+  }]
+});
+
+var container = CloudKit.getDefaultContainer();
+var database = container.publicCloudDatabase;
+
+function fetchFallacies() {
+  var query = {
+    recordType: "Fallacies",
+    sortBy: [
+      {
+        fieldName: "name",
+        ascending: true
+      }
+    ]
+  };
+  
+  var options = {
+    resultsLimit: 200
+  };
+  
+  database.performQuery(query, options).then(function(response) {
+    if (response.hasErrors) {
+      throw response.errors[0];
+    } else {
+      appendFallacies(response.records);
+    };
+  });
+};
+
+function appendFallacies(records) {
+  var recordsDiv = document.querySelector("#records");
+  var accordionElement = rootAccordionElement();
+  
+  records.forEach(record => {
+    accordionElement.append(fallacyElement(record));
+  });
+  
+  recordsDiv.append(accordionElement);
+};
+
+function rootAccordionElement() {
+  var element = document.createElement("ul");
+  element.setAttribute("uk-accordion", "");
+  return element;
+};
+
+function fallacyElement(record) {
+  const fields = record.fields;
+  
+  var accordionListElement = styledAccordionItemElement();
+  var contentElement = styledContentElement();
+  
+  if (fields.hasOwnProperty("name")) {
+    contentElement.append(attributeElement("Name", fields.name.value));
+  };
+  
+  if (fields.hasOwnProperty("latinName")) {
+    contentElement.append(attributeElement("Latin Name", fields.latinName.value));
+  };
+  
+  if (fields.hasOwnProperty("aliases")) {
+    const aliasesDetail = fields.aliases.value.join(", ").italics();
+    contentElement.append(attributeElement("Aliases", aliasesDetail));
+  };
+  
+  if (fields.hasOwnProperty("newTerminology")) {
+    contentElement.append(listElement("New Terminology", fields.newTerminology.value));
+  };
+  
+  if (fields.hasOwnProperty("logicalForms")) {
+    contentElement.append(listElement("Logical Forms", fields.logicalForms.value));
+  };
+  
+  if (fields.hasOwnProperty("description")) {
+    contentElement.append(attributeElement("Description", fields.description.value));
+  };
+  
+  if (fields.hasOwnProperty("examples")) {
+    contentElement.append(listElement("Examples", fields.examples.value));
+  };
+  
+  if (fields.hasOwnProperty("exceptions")) {
+    contentElement.append(listElement("Exceptions", fields.exceptions.value));
+  };
+  
+  if (fields.hasOwnProperty("tip")) {
+    contentElement.append(attributeElement("Tip", fields.tip.value));
+  };
+  
+  if (fields.hasOwnProperty("references")) {
+    contentElement.append(listElement("References", fields.references.value));
+  };
+  
+  accordionListElement.append(fallacyTitleElement(fields.name.value), contentElement);
+  
+  return accordionListElement;
+};
+
+function fallacyTitleElement(title) {
+  var element = document.createElement("a");
+  element.classList.add("uk-accordion-title");
+  element.innerText = title;
+  return element;
+};
+
+function styledContentElement() {
+  var element = document.createElement("div");
+  element.classList.add("uk-accordion-content");
+  return element;
+};
+
+function styledAccordionItemElement() {
+  var element = document.createElement("li");
+  element.style.backgroundColor = "silver";
+  element.style.border = "thin solid #000000";
+  element.style.borderRadius = "20px";
+  element.style.margin = "10px";
+  element.style.padding = "20px";
+  return element;
+};
+
+function attributeElement(title, detail) {
+  var element = document.createElement("p");
+  element.innerHTML = `<b>${title}</b>: ${detail}`;
+  return element;
+};
+
+function listElement(title, items) {
+  var divElement = document.createElement("div");
+  var titleElement = document.createElement("p");
+  titleElement.innerHTML = `<b>${title}</b>:`;
+  var itemsElement = document.createElement("ul");
+  items.forEach(item => {
+    var itemElement = document.createElement("li");
+    itemElement.innerText = item;
+    itemElement.style.marginBottom = "1em";
+    itemsElement.append(itemElement);
+  });
+  divElement.append(titleElement, itemsElement);
+  return divElement;
+};
+
+fetchFallacies();
