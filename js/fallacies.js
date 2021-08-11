336cd3c8a618e9494ad1bb8348f6cbe9a46778c4
Paul Tavitian <paul.tavitian@icloud.com>
2021-08-11 20:43:02 +1000

    Changes

--- js/fallacies.js
+++ js/fallacies.js
@@ -42,104 +42,102 @@ function fetchFallacies() {
 
 function appendFallacies(records) {
   var recordsDiv = document.querySelector("#records");
-  var accordionElement = styledAccordionElement();
   
   records.forEach(record => {
-    const element = fallacyElement(record);
-    accordionElement.append(element);
+    var accordionElement = document.createElement("div");
+    accordionElement.className = "accordion-item";
+    const titleElement = fallacyTitleElement(record.fields.name.value);
+    const contentElement = fallacyElement(record);
+    accordionElement.append(titleElement, contentElement);
+    recordsDiv.append(accordionElement);
   });
-  
-  recordsDiv.append(accordionElement);
-};
-
-function styledAccordionElement() {
-  var element = document.createElement("ul");
-  element.setAttribute("uk-accordion", "");
-  return element;
 };
 
 function fallacyElement(record) {
   const fields = record.fields;
-  const titleElement = fallacyTitleElement(fields.name.value);
-  var contentElement = styledContentElement();
-  var accordionItemElement = styledAccordionItemElement();
+  
+  var headingId = "heading" + fields.name.value;
+  var collapseId = "collapse" + fields.name.value;
+  
+  var divElement = document.createElement("div");
+  divElement.className = "accordion-collapse collapse";
+  divElement.setAttribute("id", collapseId);
+  divElement.setAttribute("aria-labelledby", headingId);
+  divElement.setAttribute("data-bs-parent", "#records");
+  var bodyDiv = document.createElement("div");
+  bodyDiv.className = "accordion-body";
   
   if (fields.hasOwnProperty("name")) {
     const element = attributeElement("Name", fields.name.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("latinName")) {
     const element = attributeElement("Latin Name", fields.latinName.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("aliases")) {
     const detail = fields.aliases.value.join(", ").italics();
     const element = attributeElement("Aliases", detail);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("newTerminology")) {
     const element = listElement("New Terminology", fields.newTerminology.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("logicalForms")) {
     const element = listElement("Logical Forms", fields.logicalForms.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("description")) {
     const element = attributeElement("Description", fields.description.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("examples")) {
     const element = listElement("Examples", fields.examples.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("exceptions")) {
     const element = listElement("Exceptions", fields.exceptions.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("tip")) {
     const element = attributeElement("Tip", fields.tip.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
   if (fields.hasOwnProperty("references")) {
     const element = listElement("References", fields.references.value);
-    contentElement.append(element);
+    bodyDiv.append(element);
   };
   
-  accordionItemElement.append(titleElement, contentElement);
-  return accordionItemElement;
+  divElement.append(bodyDiv);
+  return divElement;
 };
 
 function fallacyTitleElement(title) {
-  var element = document.createElement("a");
-  element.classList.add("uk-accordion-title");
-  element.innerText = title;
-  return element;
-};
-
-function styledContentElement() {
-  var element = document.createElement("div");
-  element.classList.add("uk-accordion-content");
-  return element;
-};
-
-function styledAccordionItemElement() {
-  var element = document.createElement("li");
-  element.style.backgroundColor = "silver";
-  element.style.border = "thin solid #000000";
-  element.style.borderRadius = "20px";
-  element.style.margin = "10px";
-  element.style.padding = "20px";
-  return element;
+  var headingId = "heading" + title;
+  var collapseId = "collapse" + title;
+  var headingElement = document.createElement("h2");
+  var buttonElement = document.createElement("button");
+  headingElement.className = "accordion-header";
+  headingElement.setAttribute("id", headingId);
+  buttonElement.className = "accordion-button collapsed"
+  buttonElement.setAttribute("type", "button");
+  buttonElement.setAttribute("data-bs-toggle", "collapse");
+  buttonElement.setAttribute("data-bs-target", "#" + collapseId);
+  buttonElement.setAttribute("aria-expanded", "false");
+  buttonElement.setAttribute("aria-controls", collapseId);
+  buttonElement.textContent = title;
+  headingElement.append(buttonElement);
+  return headingElement;
 };
 
 function attributeElement(title, detail) {
